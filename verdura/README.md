eea8fd2cb5387d7d79a836457e62046870bebf03
Eunsol <92962089+xooslo@users.noreply.github.com>
2023-06-08 16:12:54 +0900

    update
    메인화면 (거의)완성 내 정보 페이지 20%

---
+++ verdura/README.md
@@ -0,0 +1,17 @@
+# verdura
+
+2023 양영디지털고 3학년 1학기 개인 프로젝트
+개발자 임은솔
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ verdura/lib/mainPage.dart
@@ -0,0 +1,319 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:verdura/guidePage.dart';
+import 'package:verdura/comunityPage.dart';
+import 'package:verdura/marketPage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MainPage(),
+      routes: {
+        '/guide' : (context) => guidePage(),
+        '/comunity' : (context) => comunityPage(),
+        '/market' : (context) => marketPage()
+      },
+    );
+  }
+}
+
+Widget _buildTop() {
+  return Column(
+    children: [
+      SizedBox(
+        height: 10,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.forest,
+                  color: Colors.black54,
+                ),
+                iconSize: 60,
+              ),
+              Text(
+                '식물 가이드',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromRGBO(27, 107, 35, 1),
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            width: 50,
+          ),
+          Column(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.people_alt,
+                  color: Colors.black54,
+                ),
+                iconSize: 60,
+              ),
+              Text(
+                '식물 커뮤니티',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromRGBO(27, 107, 35, 1),
+                ),
+              )
+            ],
+          ),
+          SizedBox(
+            width: 50,
+          ),
+          Column(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.shopping_cart_rounded,
+                  color: Colors.black54,
+                ),
+                iconSize: 60,
+              ),
+              Text(
+                '식물마켓',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromRGBO(27, 107, 35, 1),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+      SizedBox(
+        height: 20,
+      ),
+      Text(
+        '이달의 사진',
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+      ),
+      SizedBox(
+        height: 10,
+      ),
+    ],
+  );
+}
+
+final dummyItems = [
+  'assets/catWithPlant.jpg',
+  'assets/plant_home_close.jpeg',
+  'assets/plant_home.jpg',
+  'assets/plant_home_2.jpg'
+];
+
+final dummyItems2 = [
+  'http://file3.instiz.net/data/cached_img/upload/2019/04/03/0/88fcfb7eefdd127ba9013d1294ff8a17.jpg',
+  'http://file3.instiz.net/data/cached_img/upload/2019/04/05/12/52f714faa9e90d2c4d483dba678bd718.jpg',
+  'http://file3.instiz.net/data/cached_img/upload/2019/04/05/12/4fbf8364c252de14e4123a798a4d6ab3.jpg',
+  'http://file3.instiz.net/data/cached_img/upload/2019/04/05/12/da42ac52aaf301f10a75002e4584f594.jpg',
+  'http://file3.instiz.net/data/cached_img/upload/2019/02/07/5/6aa9314610ed6491ca497c1b3429149a.jpg'
+];
+
+Widget _buildMiddle() {
+  return CarouselSlider(
+    options: CarouselOptions(height: 150.0, autoPlay: true),
+    items: dummyItems.map((url) {
+      return Builder(
+        builder: (BuildContext context) {
+          return Container(
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.symmetric(horizontal: 5.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10.0),
+                child: Image.asset(
+                  url,
+                  fit: BoxFit.cover,
+                ),
+              ));
+        },
+      );
+    }).toList(),
+  );
+}
+
+Widget _buildBottom() {
+  return ListView(
+    physics: NeverScrollableScrollPhysics(),
+    shrinkWrap: true,
+    children: [
+      ListTile(
+        leading: Icon(Icons.notifications_none),
+        title: Text('[이벤트] `이달의 사진` 선정 기준 안내'),
+      ),
+      ListTile(
+        leading: Icon(Icons.notifications_none),
+        title: Text('[공지] 커뮤니티 이용 수칙'),
+      ),
+      ListTile(
+        leading: Icon(Icons.notifications_none),
+        title: Text('[공지] 식물마켓 이용 시 주의사항'),
+      ),
+      ListTile(
+        leading: Icon(Icons.notifications_none),
+        title: Text('[이벤트] 6월 이달의 사진 당첨자 발표'),
+      ),
+      ListTile(
+        leading: Icon(Icons.notifications_none),
+        title: Text('[공지] 5월 오류 업데이트 공지'),
+      ),
+      ListTile(
+        leading: Icon(Icons.notifications_none),
+        title: Text('[이벤트] 5월 이달의 사진 당첨자 발표'),
+      ),
+      ListTile(
+        leading: Icon(Icons.notifications_none),
+        title: Text('[공지] 홈 화면 디자인 변경 안내'),
+      ),
+    ],
+  );
+}
+
+Widget _secondTopBuild(){
+  return Column(
+    children: [
+      SizedBox(
+        height: 20,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('내 정보 수정하기', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),),
+        ],
+      ),
+      SizedBox(
+        height: 10,
+      ),
+      CircleAvatar(
+        child: Icon(Icons.person, size: 80, color: Colors.white,),
+        backgroundColor: Colors.grey,
+        maxRadius: 50,
+      ),
+      TextButton(
+          onPressed: (){}, 
+          child: Text('프로필 사진 바꾸기')
+      ),
+
+    ],
+  );
+}
+
+class Page1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [_buildTop(), _buildMiddle(), _buildBottom()],
+      ),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [_secondTopBuild()],
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MainPage> {
+  var _index = 0;
+
+  var _pages = [
+    Page1(),
+    Page2(),
+  ];
+  var appbarColor = Color.fromRGBO(27, 107, 35, 1);
+
+  @override
+  void initState(){
+    super.initState();
+    print('MainPage initState()');
+  }
+
+  @override
+  void dispose(){
+    super.dispose();
+    print('MainPage dispose()');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Text(
+            'Vedura',
+            style: TextStyle(
+              color: appbarColor,
+            ),
+          ),
+          centerTitle: true,
+        ),
+        body: _pages[_index],
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) {
+            setState(() {
+              _index = index;
+              if(index==1){
+                appbarColor = Colors.black;
+              }else{
+                appbarColor = Color.fromRGBO(27, 107, 35, 1);
+              }
+            });
+          },
+          currentIndex: _index,
+          selectedItemColor: appbarColor,
+          unselectedItemColor: Colors.grey,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle), label: '내 정보'),
+          ],
+        ),
+      ),
+    );
+  }
+}
