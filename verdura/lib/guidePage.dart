5d88b6070af2a545b975ea14cc86121d93ff2eb4
xooslo <ys2110205@y-y.hs.kr>
2023-06-19 02:41:32 +0900

    update

--- verdura/lib/guidePage.dart
+++ verdura/lib/guidePage.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:verdura/guidePage.dart';
-import 'package:verdura/comunityPage.dart';
-import 'package:verdura/marketPage.dart';
-import 'package:verdura/loginPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,13 +25,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
       ),
       home: guidePage(),
-      routes: {
-        '/guide': (context) => guidePage(),
-        '/comunity': (context) => comunityPage(),
-        '/market': (context) => marketPage(),
-        '/login': (context) => loginPage(),
-        '/albo' : (context) => alboMonstera(),
-      },
     );
   }
 }
@@ -64,7 +53,7 @@ class _guidePageState extends State<guidePage> {
     'assets/albo-monstera.jpeg',
     'assets/monstera.jpeg',
     'assets/caladea.jpeg',
-    'assets/dayuk.jpeg',
+    'assets/dayuk.png',
     'assets/table-yaga.jpeg',
     'assets/table-yaga-2.jpeg',
   ];
@@ -98,8 +87,8 @@ class _guidePageState extends State<guidePage> {
                     padding: EdgeInsets.all(10),
                     child: InkWell(
                       onTap: () async {
-                        final result = await Navigator.pushNamed(context, '/albo');
-                        print(result);
+                        Navigator.push(context, MaterialPageRoute(builder: (context) =>
+                            AlboMonstera()));
                         },
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(10),
@@ -113,6 +102,10 @@ class _guidePageState extends State<guidePage> {
                   Padding(
                     padding: EdgeInsets.all(10),
                     child: InkWell(
+                      onTap: () async {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) =>
+                            Monstera()));
+                      },
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(10),
                         child: Image.asset(
@@ -125,6 +118,10 @@ class _guidePageState extends State<guidePage> {
                   Padding(
                     padding: EdgeInsets.all(10),
                     child: InkWell(
+                      onTap: () async {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) =>
+                            Caladea()));
+                      },
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(10),
                         child: Image.asset(
@@ -137,6 +134,10 @@ class _guidePageState extends State<guidePage> {
                   Padding(
                     padding: EdgeInsets.all(10),
                     child: InkWell(
+                      onTap: () async {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) =>
+                            Dayuk()));
+                      },
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(10),
                         child: Image.asset(
@@ -179,14 +180,14 @@ class _guidePageState extends State<guidePage> {
   }
 }
 
-class alboMonstera extends StatefulWidget {
-  const alboMonstera({super.key});
+class AlboMonstera extends StatefulWidget {
+  const AlboMonstera({super.key});
 
   @override
-  State<alboMonstera> createState() => _alboMonsteraState();
+  State<AlboMonstera> createState() => _AlboMonsteraState();
 }
 
-class _alboMonsteraState extends State<alboMonstera> {
+class _AlboMonsteraState extends State<AlboMonstera> {
   var appbarColor = Color.fromRGBO(27, 107, 35, 1);
 
   @override
@@ -200,13 +201,471 @@ class _alboMonsteraState extends State<alboMonstera> {
           onPressed: (){
             Navigator.pop(context, 'albo-momstera test');
           },
-          icon: Icon(Icons.navigate_before),
+          icon: Icon(Icons.navigate_before, color: appbarColor,),
         ),
       ),
-      body: Column(
-        children: [
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/albo-monstera.jpeg',
+              width: 400,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              '알보 몬스테라',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              '몬스테라 알보 바리에가타',
+              style: TextStyle(fontSize: 20, color: Colors.grey),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.search), Text('MORE')],
+                ),
+                SizedBox(
+                  width: 60,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.send), Text('ROUTE')],
+                ),
+                SizedBox(
+                  width: 60,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.share), Text('SHARE')],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 15,
+                ),
+                Icon(Icons.auto_awesome, size: 40,),
+                SizedBox(
+                  width: 10,
+                ),
+                Text('알보 몬스테라란?', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)
+              ],
+            ),
+            Container(
+              width: 360,
+              child: Text('알보 몬스테라는 유전적인 변이로 인해 초록색을 띠는 엽록소가 없이 부분 부분마다 하얀 무늬가 생긴 변이종이에요. 인위적으로는 만들어 낼 수 없고, 수입도 금지되어 있어 희소성이 높아 아주 비싼 가격을 형성하고 있답니다. `식테크`, `무천이(무늬천재)`,\n `알보몬` 등의 많은 신조어를 만들어 낸 장본인인 알보 몬스테라는 흰 부분이 많을수록 햇빛을 흡수하여 필요한 양분을 만들어내는 광합성작용을 잘 하지 못하기 때문에 일반 몬스테라보다 더 많은 빛을 필요로 해요. 또한 온도와 습도에도 아주 예민한 아이라 많은 식집사들의 애간장을 태우기도 한답니다. 알보 몬스테라는 배수가 아주 잘 되는 토양에 심어주시고, 창문을 거친 밝은 간접광이 드는 따뜻한 곳에서 공기를 촉촉하게 유지해주면서 키워주셔야 한답니다. 잎 크기를 크게 키우시고 싶다면 수태봉의 도움을 받아보세요. 더 튼튼한 잎으로 성장할 수 있답니다.',
+                  style: TextStyle(
+                    fontSize: 17,
+                  )),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 15,
+                ),
+                Icon(Icons.auto_awesome, size: 40,),
+                SizedBox(
+                  width: 10,
+                ),
+                Text('알보 몬스테라 관리를 위한 팁', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)
+              ],
+            ),
+            Container(
+              width: 360,
+              child: Text('1. 물은 평균 주 1~2회, 흙 표면부터 3cm까지 마르면 듬뿍 주세요\n \n2. 반양지 식물으로 하루 2~3시간 정도의 은은한 햇빛이 필요해요\n \n3. 권장 습도는 70% 이상으로 주변 공기를 촉촉하게 관리해주세요\n \n4. 20~25℃의 온도에서 잘 자라요', style: TextStyle(fontSize: 17),),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-        ],
+class Monstera extends StatefulWidget {
+  const Monstera({super.key});
+
+  @override
+  State<Monstera> createState() => _MonsteraState();
+}
+
+class _MonsteraState extends State<Monstera> {
+  var appbarColor = Color.fromRGBO(27, 107, 35, 1);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text('몬스테라 키우는 법', style: TextStyle(color: appbarColor),),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context, 'momstera test');
+          },
+          icon: Icon(Icons.navigate_before, color: appbarColor,),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/monstera.jpeg',
+              width: 400,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              '몬스테라',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              '몬스테라 델리시오사',
+              style: TextStyle(fontSize: 20, color: Colors.grey),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.search), Text('MORE')],
+                ),
+                SizedBox(
+                  width: 60,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.send), Text('ROUTE')],
+                ),
+                SizedBox(
+                  width: 60,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.share), Text('SHARE')],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 15,
+                ),
+                Icon(Icons.auto_awesome, size: 40,),
+                SizedBox(
+                  width: 10,
+                ),
+                Text('몬스테라 델리시오사란?',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)
+              ],
+            ),
+            Container(
+              width: 360,
+              child: Text(
+                  '남미 열대 우림 출신의 몬스테라 델리시오사는 큰 나무를 타고 올라가는 습성이 있는 덩굴성 식물이에요. 하트 모양의 찢어진 특이한 잎 모양이 시원한 느낌을 자아내고, 키우기도 까다롭지 않아 사랑받고 있어요. 몬스테라는 아래 쪽의 잎에도 빛을 나누어주기 위해 잎이 찢어졌다고 해요. 천남성과에 속하므로 독성이 있어 아이와 반려동물이 있다면 섭취하지 않도록 주의를 기울여야 하는 친구예요.',
+                  style: TextStyle(
+                    fontSize: 17,
+                  )),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 15,
+                ),
+                Icon(Icons.auto_awesome, size: 40,),
+                SizedBox(
+                  width: 10,
+                ),
+                Text('몬스테라 관리를 위한 팁',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)
+              ],
+            ),
+            Container(
+              width: 360,
+              child: Text(
+                '1. 물은 평균 주 1~2회, 흙 표면부터 3cm까지 마르면 듬뿍 주세요\n \n2. 반양지 식물으로 하루 2~3시간 정도의 은은한 햇빛이 필요해요\n \n3. 권장 습도는 70% 이상으로 주변 공기를 촉촉하게 관리해주세요\n \n4. 20~25℃의 온도에서 잘 자라요',
+                style: TextStyle(fontSize: 17),),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Caladea extends StatefulWidget {
+  const Caladea({super.key});
+
+  @override
+  State<Caladea> createState() => _CaladeaState();
+}
+
+class _CaladeaState extends State<Caladea> {
+  var appbarColor = Color.fromRGBO(27, 107, 35, 1);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text('칼라데아 키우는 법', style: TextStyle(color: appbarColor),),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context, 'caladea test');
+          },
+          icon: Icon(Icons.navigate_before, color: appbarColor,),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/caladea.jpeg',
+              width: 400,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              '칼라데아',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              '칼라데아 퓨전화이트',
+              style: TextStyle(fontSize: 20, color: Colors.grey),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.search), Text('MORE')],
+                ),
+                SizedBox(
+                  width: 60,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.send), Text('ROUTE')],
+                ),
+                SizedBox(
+                  width: 60,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.share), Text('SHARE')],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 15,
+                ),
+                Icon(Icons.auto_awesome, size: 40,),
+                SizedBox(
+                  width: 10,
+                ),
+                Text('칼라데아 퓨전화이트란?',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)
+              ],
+            ),
+            Container(
+              width: 360,
+              child: Text(
+                  '녹색과 흰색의 대비가 아름다운 칼라데아 퓨전화이트는 잎 뒷면이 마젠타색을 띠는 반전 매력을 소유한 마란타과의 식물입니다. 칼라데아의 한 종류답게 퓨전화이트 또한 따뜻한 온도와 촉촉한 공기를 좋아합니다. 퓨전화이트 하나만으로도 존재감이 뛰어나 온 집 안을 분위기 있게 만들어줘요. 다른 칼라데아들보다 잎이 얇은 편이라 세심하게 습도를 조절해주어야 하는 식물이지만, 아름다운 외모와 희소성 덕분에 아주 인기가 많은 품종이랍니다. 또한 독성이 없어 반려동물을 기르시는 집사님들도 걱정 없이 키울 수 있는 식물이랍니다!',
+                  style: TextStyle(
+                    fontSize: 17,
+                  )),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 15,
+                ),
+                Icon(Icons.auto_awesome, size: 40,),
+                SizedBox(
+                  width: 10,
+                ),
+                Text('칼라데아 관리를 위한 팁',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)
+              ],
+            ),
+            Container(
+              width: 360,
+              child: Text(
+                '1. 물은 평균 주 1~2회, 흙 표면부터 3cm까지 마르면 듬뿍 주세요\n \n2. 반양지 식물으로 하루 2~3시간 정도의 은은한 햇빛이 필요해요\n \n3. 권장 습도는 70% 이상으로 주변 공기를 촉촉하게 관리해주세요\n \n4. 16~25℃의 온도에서 잘 자라요',
+                style: TextStyle(fontSize: 17),),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Dayuk extends StatefulWidget {
+  const Dayuk({super.key});
+
+  @override
+  State<Dayuk> createState() => _DayukState();
+}
+
+class _DayukState extends State<Dayuk> {
+  var appbarColor = Color.fromRGBO(27, 107, 35, 1);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text('제옥(다육이) 키우는 법', style: TextStyle(color: appbarColor),),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context, 'dayuk test');
+          },
+          icon: Icon(Icons.navigate_before, color: appbarColor,),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/caladea.jpeg',
+              width: 400,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              '제옥',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              '제옥 (플레이오스필로스 넬리)',
+              style: TextStyle(fontSize: 20, color: Colors.grey),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.search), Text('MORE')],
+                ),
+                SizedBox(
+                  width: 60,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.send), Text('ROUTE')],
+                ),
+                SizedBox(
+                  width: 60,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.share), Text('SHARE')],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 15,
+                ),
+                Icon(Icons.auto_awesome, size: 40,),
+                SizedBox(
+                  width: 10,
+                ),
+                Text('칼라데아 퓨전화이트란?',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)
+              ],
+            ),
+            Container(
+              width: 360,
+              child: Text(
+                  '녹색과 흰색의 대비가 아름다운 칼라데아 퓨전화이트는 잎 뒷면이 마젠타색을 띠는 반전 매력을 소유한 마란타과의 식물입니다. 칼라데아의 한 종류답게 퓨전화이트 또한 따뜻한 온도와 촉촉한 공기를 좋아합니다. 퓨전화이트 하나만으로도 존재감이 뛰어나 온 집 안을 분위기 있게 만들어줘요. 다른 칼라데아들보다 잎이 얇은 편이라 세심하게 습도를 조절해주어야 하는 식물이지만, 아름다운 외모와 희소성 덕분에 아주 인기가 많은 품종이랍니다. 또한 독성이 없어 반려동물을 기르시는 집사님들도 걱정 없이 키울 수 있는 식물이랍니다!',
+                  style: TextStyle(
+                    fontSize: 17,
+                  )),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 15,
+                ),
+                Icon(Icons.auto_awesome, size: 40,),
+                SizedBox(
+                  width: 10,
+                ),
+                Text('칼라데아 관리를 위한 팁',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),)
+              ],
+            ),
+            Container(
+              width: 360,
+              child: Text(
+                '1. 물은 평균 주 1~2회, 흙 표면부터 3cm까지 마르면 듬뿍 주세요\n \n2. 반양지 식물으로 하루 2~3시간 정도의 은은한 햇빛이 필요해요\n \n3. 권장 습도는 70% 이상으로 주변 공기를 촉촉하게 관리해주세요\n \n4. 16~25℃의 온도에서 잘 자라요',
+                style: TextStyle(fontSize: 17),),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
       ),
     );
   }
